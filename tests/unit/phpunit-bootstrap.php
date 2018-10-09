702ba36127597383d32f6b74cd0557e0bca526aa
Wojciech Peisert <wojtekp@sauron.pl>
2018-10-09 14:45:18 +0200

    unit tests skeleton

---
+++ tests/unit/phpunit-bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @category    ClassificationstoreBundle
+ * @date        09/10/2018 10:15
+ * @author      Wojciech Peisert <wpeisert@divante.co>
+ * @copyright   Copyright (c) 2018 Divante Ltd. (https://divante.co/)
+ */
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+\Pimcore\Bootstrap::setProjectRoot();
+\Pimcore\Bootstrap::boostrap();
