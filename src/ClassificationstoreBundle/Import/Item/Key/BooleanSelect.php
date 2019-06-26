e133d0be84c947924078f05a132d8ed61e9de7f9
Agata Drozdek <adrozdek@divante.pl>
2019-06-26 21:47:59 +0200

    Added default config for booleanSelect.

--- src/ClassificationstoreBundle/Import/Item/Key/BooleanSelect.php
+++ src/ClassificationstoreBundle/Import/Item/Key/BooleanSelect.php
@@ -3,18 +3,32 @@
  * @category    ClassificationstoreBundle
  * @date        28/09/2018 10:15
  * @author      Wojciech Peisert <wpeisert@divante.co>
+ * @author      Agata Drozdek <adrozdek@divante.pl>
  * @copyright   Copyright (c) 2018 Divante Ltd. (https://divante.co/)
  */
 
 namespace Divante\ClassificationstoreBundle\Import\Item\Key;
 
-use Divante\ClassificationstoreBundle\Import\Item\Key;
+use Divante\ClassificationstoreBundle\Constants;
+use Divante\ClassificationstoreBundle\Import\Item\KeyWithDefaults;
 
 /**
  * Class BooleanSelect
  * @package Divante\ClassificationstoreBundle\Import\Item\Key
  */
-class BooleanSelect extends Key
+class BooleanSelect extends KeyWithDefaults
 {
+    /**
+     * @return array
+     */
+    public function getDefaultData(): array
+    {
+        $options = [
+            Constants::DEFINITION_YES_LABEL   => 'yes',
+            Constants::DEFINITION_NO_LABEL    => 'no',
+            Constants::DEFINITION_EMPTY_LABEL => 'empty',
+        ];
 
+        return $options;
+    }
 }
